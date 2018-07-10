f1910dad714eed60444a94a098f5185949bff028
lida12 <lidajacob4@gmail.com>
2018-07-10 15:45:37 +0530

    assignment

--- day1/q1.php
+++ day1/q1.php
@@ -3,5 +3,5 @@ echo "'I'm about to learn PHP!'<br>";
 echo "'You could use Zend\Symfony'<br>";
 echo "\"It's called\"Product Tree\"\"<br>";
 echo "\"\\0 is called null character\"<br>";
-
+echo "hi";
 ?>
